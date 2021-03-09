3c57e6208557eb3c59e9519a080082cc43937ead
Ben Dosch <Benjamindosch@gmail.com>
2021-03-09 16:25:19 +0000

    switching branches

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+
+/**
+* binary_tree_node - Function that makes a binary tree node
+* @parent: pointer to parent node
+* @value: value to store at node
+* Return: a pointer to new node on sucess, NULL on fail.
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	new = malloc(sizeof(binary_tree_t) * 1);
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
