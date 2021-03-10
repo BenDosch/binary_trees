6e3a2136c678a4827bde66b551d7ec37933347bc
Ben Dosch <Benjamindosch@gmail.com>
2021-03-10 16:04:19 +0000

    applying func to node

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,7 +13,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		printf("%d\n", tree->n);
+		func(tree);
 		if (tree->left)
 			binary_tree_preorder(tree->left, func);
 		if (tree->right)
