d0f77770dfc7d0945bf4c7e0e2aee5ff653c2acf
Ben Dosch <Benjamindosch@gmail.com>
2021-03-10 20:38:17 +0000

    updates to check height in 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,19 +8,14 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-<<<<<<< HEAD
-	if(tree)
-	{
-		if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right) &&
-=======
 	if (tree)
 	{
 		if (tree->left && tree->right &&
 			binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right) &&
->>>>>>> Sang
 			binary_tree_balance(tree->left) == 0 &&
-			binary_tree_balance(tree->right) == 0)
+			binary_tree_balance(tree->right) == 0 &&
+			binary_tree_height(tree->left) == binary_tree_height(tree->right))
 			return (1);
 	}
 	return (0);
