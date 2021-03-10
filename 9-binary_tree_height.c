5eb79a6e660fa9a836125e2e7f48d22465428f80
Ben Dosch <Benjamindosch@gmail.com>
2021-03-10 16:49:40 +0000

    ready for checker

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int lh = 0, rh = 0;
+	size_t lh = 0, rh = 0;
 
 	if (tree)
 	{
 		if (tree->left)
-			lh = binary_tree_height(tree->left);
+			lh = 1 + binary_tree_height(tree->left);
 		if (tree->right)
-			rh = binary_tree_height(tree->right);
+			rh = 1 + binary_tree_height(tree->right);
 		if (lh >= rh && lh != 0)
-			return (lh + 1);
+			return (lh);
 		else if (rh > lh && rh != 0)
-			return (rh + 1);
+			return (rh);
 	}
 	return (0);
 }
