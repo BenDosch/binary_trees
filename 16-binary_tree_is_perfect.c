247d7a8068655b71563eec3b161689c41850ff75
Ben Dosch <Benjamindosch@gmail.com>
2021-03-10 19:14:16 +0000

    tested 16 fixes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,11 +8,13 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-
-	if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right) &&
-		binary_tree_balance(tree->left) == 0 &&
-		binary_tree_balance(tree->right) == 0)
-		return (1);
+	if(tree)
+	{
+		if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right) &&
+			binary_tree_balance(tree->left) == 0 &&
+			binary_tree_balance(tree->right) == 0)
+			return (1);
+	}
 	return (0);
 }
 
