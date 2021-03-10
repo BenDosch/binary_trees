61443d0660b6cd4c783b0af3433e87513db431c9
sang-nguy0920 <2321@holbertonschool.com>
2021-03-10 17:32:42 +0000

    corrected code for 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,13 +12,29 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree)
 	{
+		total += binary_tree_is_leaf(tree);
 		if (tree->left)
-			total += binary_tree_is_leaf(tree->left);
 			total += binary_tree_leaves(tree->left);
 		if (tree->right)
-			total += binary_tree_is_leaf(tree->right);
 			total += binary_tree_leaves(tree->right);
 		return (total);
 	}
 	return (0);
+}
+
+/**
+* binary_tree_is_leaf - function that checks if a node is a leaf
+* @node: pointer to the node to check
+*
+* Return: 1 if node is a leaf, otherwise 0
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
